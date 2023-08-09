d52358488bf3caacd745e9d3f2818aa1713e3920
sillock1 <jprest97@gmail.com>
2023-08-09 21:21:24 +0100

    chore: changed container network group name and syslog

--- config-parts/containers.sh
+++ config-parts/containers.sh
@@ -1,12 +1,12 @@
 #!/bin/vbash
 
 # Container networks
-set container network containers prefix '10.5.0.0/24'
+set container network CONTAINERS prefix '10.5.0.0/24'
 
 # haproxy-k8s-api
 set container name haproxy-k8s-api image 'docker.io/library/haproxy:2.8.1'
 set container name haproxy-k8s-api memory '0'
-set container name haproxy-k8s-api network containers address '10.5.0.2'
+set container name haproxy-k8s-api network CONTAINERS address '10.5.0.2'
 set container name haproxy-k8s-api restart 'on-failure'
 set container name haproxy-k8s-api shared-memory '0'
 set container name haproxy-k8s-api volume config source '/config/containers/haproxy/config/haproxy.cfg'
@@ -18,7 +18,7 @@ set container name bind cap-add 'net-bind-service'
 set container name bind image 'docker.io/internetsystemsconsortium/bind9:9.19'
 set container name bind command '/usr/sbin/named -4 -f -c /etc/bind/named.conf -u bind'
 set container name bind memory '0'
-set container name bind network containers address '10.5.0.3'
+set container name bind network CONTAINERS address '10.5.0.3'
 set container name bind restart 'on-failure'
 set container name bind shared-memory '0'
 set container name bind volume config destination '/etc/bind'
@@ -34,7 +34,7 @@ set container name dnsdist environment TZ value 'Europe/London'
 set container name dnsdist image 'docker.io/powerdns/dnsdist-18:1.8.0'
 set container name dnsdist arguments '--log-timestamps'
 set container name dnsdist memory '0'
-set container name dnsdist network containers address '10.5.0.4'
+set container name dnsdist network CONTAINERS address '10.5.0.4'
 set container name dnsdist restart 'on-failure'
 set container name dnsdist shared-memory '0'
 set container name dnsdist volume config source '/config/containers/dnsdist/config/dnsdist.conf'
--- config-parts/system.sh
+++ config-parts/system.sh
@@ -11,5 +11,10 @@ set system login user vyos authentication public-keys personal type 'ssh-ed25519
 set system name-server '1.1.1.1'
 
 set system sysctl parameter kernel.pty.max value '24000'
+set system syslog global facility all level info
+set system syslog host 10.45.0.2 facility kern level 'warning'
+set system syslog host 10.45.0.2 protocol 'tcp'
+set system syslog host 10.45.0.2 port '6001'
+set system syslog host 10.45.0.2 format 'octet-counted'
 
 set system time-zone 'Europe/London'
