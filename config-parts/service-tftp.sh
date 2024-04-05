df11c4f0074eca38d2c62fa6cdb1b3fd2c6a809b
sillock1 <jprest97@gmail.com>
2024-04-05 20:48:06 +0100

    fix: mkdir tftp boot

--- config-parts/service-tftp.sh
+++ config-parts/service-tftp.sh
@@ -1,5 +1,6 @@
 set service tftp-server directory '/config/tftpboot'
 set service tftp-server listen-address 10.1.0.1
+mkdir -p /config/tftpboot
 curl -L http://boot.ipxe.org/ipxe.efi -o /config/tftpboot/ipxe.efi
 curl -L http://boot.ipxe.org/undionly.kpxe -o /config/tftpboot/undionly.kpxe
 sudo chown -R tftp:tftp /config/tftpboot/
